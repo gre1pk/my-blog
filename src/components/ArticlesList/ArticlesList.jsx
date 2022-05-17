65261634d657cc4620c93ee309be30abe718b16a
gre1pk <raik1996@gmail.com>
2022-05-17 18:33:35 +0300

    feat: add pagination

--- src/components/ArticlesList/ArticlesList.jsx
+++ src/components/ArticlesList/ArticlesList.jsx
@@ -1,18 +1,44 @@
+import { useEffect } from 'react'
 import { Pagination } from 'antd'
+import { useDispatch, useSelector } from 'react-redux'
 
 import Article from '../Article'
+import getArtickles from '../../store/asyncActions/articlesThunks'
+import { setPages } from '../../store/actions/articlesAction'
 
 import classes from './ArticlesList.module.scss'
 
 function ArticlesList() {
+  const dispatch = useDispatch()
+  const { articles, articlesCount, currentPages } = useSelector((store) => store.articlesReduser)
+
+  useEffect(() => {
+    dispatch(getArtickles(currentPages))
+  }, [dispatch, currentPages])
+
+  const onCurrentPages = (value) => {
+    dispatch(setPages(value))
+  }
+
+  const articleList = articles.map((el) => {
+    return (
+      <Article
+        key={el.slug}
+        title={el.title}
+        description={el.description}
+        createdAt={el.createdAt}
+        tagList={el.tagList}
+        favoritesCount={el.favoritesCount}
+        author={el.author}
+      />
+    )
+  })
+
   return (
     <>
-      <div className={classes.articleList}>
-        <Article />
-        <Article />
-      </div>
+      <div className={classes.articleList}>{articleList}</div>
       <div className={classes.pagination}>
-        <Pagination total={50} />
+        <Pagination total={articlesCount} showSizeChanger={false} current={currentPages} onChange={onCurrentPages} />
       </div>
     </>
   )
---
+++ src/services/articlesServices.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+class ArticlesServices {
+  baseUrl = 'https://kata.academy:8021'
+
+  async getResponce(url) {
+    try {
+      const result = await axios.get(`${this.baseUrl}/${url}`)
+      if (result.statusText !== 'OK') {
+        throw new Error(result.status)
+      }
+
+      return result.data
+    } catch (err) {
+      throw new Error(err)
+    }
+  }
+
+  async getArticles(offset = 0) {
+    const res = await this.getResponce(`api/articles?limit=5&offset=${offset}`)
+    return res
+  }
+}
+
+export default ArticlesServices
