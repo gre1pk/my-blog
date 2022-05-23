39044ed9ba29ccadcf5255e48f820936bbe5982c
gre1pk <raik1996@gmail.com>
2022-05-23 23:23:32 +0300

    feat: add redax-persit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
-import store from './store'
+import store, { persistor } from './store'
 import App from './components/App'
 
 import './scss/style.scss'
@@ -10,6 +11,8 @@ import './scss/style.scss'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 )
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,23 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import thunk from 'redux-thunk'
 
 import articlesReducer from './reducers/articlesReducer'
 import articleReducer from './reducers/articleReducer'
 import userReducer from './reducers/userReducer'
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
 const rootReducer = combineReducers({ articlesReducer, articleReducer, userReducer })
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
+const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+export const persistor = persistStore(store)
 export default store
